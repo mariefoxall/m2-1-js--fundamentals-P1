67d0f645bfddaae4aa8d05ec2037e63c90fe3a7a
Marie Foxall <marie@wastedeffort.ca>
2020-06-15 16:06:20 -0400

    exercise 6.5

--- workshop/exercise-6.5.js
+++ workshop/exercise-6.5.js
@@ -9,3 +9,11 @@
 // ...
 
 // many other possibilities here.
+
+for (let number = 0; number <= 15; number++) {
+  if (number % 2 === 0) {
+    console.log(`${number} is even`);
+  } else {
+    console.log(`${number} is odd`);
+  }
+}
