5cf7cbabbd53b7a9b1f62a4d1289ee6d052d00af
Marie Foxall <marie@wastedeffort.ca>
2020-06-15 16:56:25 -0400

    exercise 11

--- workshop/exercise-11-stretch.js
+++ workshop/exercise-11-stretch.js
@@ -3,6 +3,17 @@
 // using newline characters to separate lines. At each position of the grid
 // there is either an "_" or a "#" character. The characters should form a chessboard.
 
+let grid = "";
+
+for (let row = 1; row <= 8; row++) {
+  if (row % 2 === 0) {
+    grid = grid + "_#_#_#_#\n";
+  } else {
+    grid = grid + "#_#_#_#_\n";
+  }
+}
+console.log(grid);
+
 // #_#_#_#_
 // _#_#_#_#
 // #_#_#_#_
