145323d77a8eb4baa11b1d4a425bef79bbd82d9f
Marie Foxall <marie@wastedeffort.ca>
2020-06-15 16:25:21 -0400

    exercise 9

--- workshop/exercise-9.js
+++ workshop/exercise-9.js
@@ -5,6 +5,18 @@
 //   - If the number is divisible by 3 AND 5 (eg. 15), print "FizzBuzz"
 //   - For all other numbers, print the number itself.
 
+for (let number = 1; number <= 100; number++) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (number % 3 === 0) {
+    console.log("Fizz");
+  } else if (number % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(number);
+  }
+}
+
 // e.g.
 //
 // 1
