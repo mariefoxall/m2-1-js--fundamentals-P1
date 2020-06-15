3b91c1ef67f6aaf2daa2c8a531c666d7a81ae208
Marie Foxall <marie@wastedeffort.ca>
2020-06-15 15:54:28 -0400

    Exercise 4

--- workshop/exercise-4.js
+++ workshop/exercise-4.js
@@ -5,6 +5,7 @@
 // eg:
 // 0, 1, 4, 9, 16, ...
 
-for (let number = 0; number < 12; number++) {
-  console.log('the square of ', number, ' is ', square);
+for (let number = 0; number <= 12; number++) {
+  let square = number * number;
+  console.log("the square of ", number, " is ", square);
 }
